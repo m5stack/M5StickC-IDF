1797ea1854324421ae1a903d28f4fe043bbcfc82
Hades2001 <1521377294@qq.com>
2019-10-22 09:57:24 +0800

    updata

--- components/m5stickc-idf/util/MPU6886.c
+++ components/m5stickc-idf/util/MPU6886.c
@@ -5,203 +5,202 @@ enum Ascale Acscale = AFS_8G;
 
 void MPU6886I2C_Read_NBytes(uint8_t driver_Addr, uint8_t start_Addr, uint8_t number_Bytes, uint8_t *read_Buffer)
 {
-  	I2CreadBuff(&wire0, driver_Addr, start_Addr, read_Buffer, number_Bytes);      
+    I2CreadBuff(&wire0, driver_Addr, start_Addr, read_Buffer, number_Bytes);
 }
 
 void MPU6886I2C_Write_NBytes(uint8_t driver_Addr, uint8_t start_Addr, uint8_t number_Bytes, uint8_t *write_Buffer)
 {
-	I2Cwirtebuff(&wire0, driver_Addr, start_Addr, write_Buffer, number_Bytes);
+    I2Cwirtebuff(&wire0, driver_Addr, start_Addr, write_Buffer, number_Bytes);
 }
 
-int MPU6886Init(void){
-	uint8_t tempdata[1];
-	uint8_t regdata;
-	
-	MPU6886I2C_Read_NBytes(MPU6886_ADDRESS, MPU6886_WHOAMI, 1, tempdata);
-	if(tempdata[0] != 0x19)
-		return -1;
-	vTaskDelay(5/portTICK_PERIOD_MS);
-	
-	regdata = 0x00;
-	MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_PWR_MGMT_1, 1, &regdata);
-	vTaskDelay(10 /portTICK_PERIOD_MS);
-
-	regdata = (0x01<<7);
-	MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_PWR_MGMT_1, 1, &regdata);
-	vTaskDelay(10 /portTICK_PERIOD_MS);
-
-	regdata = (0x01<<0);
-	MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_PWR_MGMT_1, 1, &regdata);
-	vTaskDelay(10 /portTICK_PERIOD_MS);
-
-	regdata = 0x10;
-	MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_ACCEL_CONFIG, 1, &regdata);
-	vTaskDelay(10 /portTICK_PERIOD_MS);
-
-	regdata = 0x18;
-	MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_GYRO_CONFIG, 1, &regdata);
-	vTaskDelay(10 /portTICK_PERIOD_MS);
-
-	regdata = 0x01;
-	MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_CONFIG, 1, &regdata);
-	vTaskDelay(10 /portTICK_PERIOD_MS);
-
-	regdata = 0x05;
-	MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_SMPLRT_DIV, 1,&regdata);
-	vTaskDelay(10 /portTICK_PERIOD_MS);
-
-	regdata = 0x00;
-	MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_INT_ENABLE, 1, &regdata);
-	vTaskDelay(10 /portTICK_PERIOD_MS);
-
-	regdata = 0x00;
-	MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_ACCEL_CONFIG2, 1, &regdata);
-	vTaskDelay(10 /portTICK_PERIOD_MS);
-
-	regdata = 0x00;
-	MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_USER_CTRL, 1, &regdata);
-	vTaskDelay(10 /portTICK_PERIOD_MS);
-
-	regdata = 0x00;
-	MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_FIFO_EN, 1, &regdata);
-	vTaskDelay(10 /portTICK_PERIOD_MS);
-
-	regdata = 0x22;
-	MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_INT_PIN_CFG, 1, &regdata);
-	vTaskDelay(10 /portTICK_PERIOD_MS);
-
-	regdata = 0x01;
-	MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_INT_ENABLE, 1, &regdata);
-
-	vTaskDelay(100 /portTICK_PERIOD_MS);
-	MPU6886getGres();
-	MPU6886getAres();
-	return 0;
-}
-
-void MPU6886getAccelAdc(int16_t* ax, int16_t* ay, int16_t* az)
+int MPU6886Init(void)
 {
-   uint8_t buf[6];  
-   MPU6886I2C_Read_NBytes(MPU6886_ADDRESS,MPU6886_ACCEL_XOUT_H,6,buf);
-   
-   *ax=((int16_t)buf[0]<<8)|buf[1];
-   *ay=((int16_t)buf[2]<<8)|buf[3];
-   *az=((int16_t)buf[4]<<8)|buf[5];
+    uint8_t tempdata[1];
+    uint8_t regdata;
 
+    MPU6886I2C_Read_NBytes(MPU6886_ADDRESS, MPU6886_WHOAMI, 1, tempdata);
+    if (tempdata[0] != 0x19)
+        return -1;
+    vTaskDelay(5 / portTICK_PERIOD_MS);
+
+    regdata = 0x00;
+    MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_PWR_MGMT_1, 1, &regdata);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+
+    regdata = (0x01 << 7);
+    MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_PWR_MGMT_1, 1, &regdata);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+
+    regdata = (0x01 << 0);
+    MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_PWR_MGMT_1, 1, &regdata);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+
+    regdata = 0x10;
+    MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_ACCEL_CONFIG, 1, &regdata);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+
+    regdata = 0x18;
+    MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_GYRO_CONFIG, 1, &regdata);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+
+    regdata = 0x01;
+    MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_CONFIG, 1, &regdata);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+
+    regdata = 0x05;
+    MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_SMPLRT_DIV, 1, &regdata);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+
+    regdata = 0x00;
+    MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_INT_ENABLE, 1, &regdata);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+
+    regdata = 0x00;
+    MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_ACCEL_CONFIG2, 1, &regdata);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+
+    regdata = 0x00;
+    MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_USER_CTRL, 1, &regdata);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+
+    regdata = 0x00;
+    MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_FIFO_EN, 1, &regdata);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+
+    regdata = 0x22;
+    MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_INT_PIN_CFG, 1, &regdata);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+
+    regdata = 0x01;
+    MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_INT_ENABLE, 1, &regdata);
+
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+    MPU6886getGres();
+    MPU6886getAres();
+    return 0;
 }
-void MPU6886getGyroAdc(int16_t* gx, int16_t* gy, int16_t* gz){
 
-  uint8_t buf[6];
-  MPU6886I2C_Read_NBytes(MPU6886_ADDRESS,MPU6886_GYRO_XOUT_H,6,buf);
-  
-  *gx=((uint16_t)buf[0]<<8)|buf[1];  
-  *gy=((uint16_t)buf[2]<<8)|buf[3];  
-  *gz=((uint16_t)buf[4]<<8)|buf[5];
-  
+void MPU6886getAccelAdc(int16_t *ax, int16_t *ay, int16_t *az)
+{
+    uint8_t buf[6];
+    MPU6886I2C_Read_NBytes(MPU6886_ADDRESS, MPU6886_ACCEL_XOUT_H, 6, buf);
+
+    *ax = ((int16_t)buf[0] << 8) | buf[1];
+    *ay = ((int16_t)buf[2] << 8) | buf[3];
+    *az = ((int16_t)buf[4] << 8) | buf[5];
+}
+void MPU6886getGyroAdc(int16_t *gx, int16_t *gy, int16_t *gz)
+{
+
+    uint8_t buf[6];
+    MPU6886I2C_Read_NBytes(MPU6886_ADDRESS, MPU6886_GYRO_XOUT_H, 6, buf);
+
+    *gx = ((uint16_t)buf[0] << 8) | buf[1];
+    *gy = ((uint16_t)buf[2] << 8) | buf[3];
+    *gz = ((uint16_t)buf[4] << 8) | buf[5];
 }
 
 void MPU6886getTempAdc(int16_t *t)
 {
-  uint8_t buf[2];  
-  MPU6886I2C_Read_NBytes(MPU6886_ADDRESS,MPU6886_TEMP_OUT_H,2,buf);
-  
-  *t=((uint16_t)buf[0]<<8)|buf[1];  
+    uint8_t buf[2];
+    MPU6886I2C_Read_NBytes(MPU6886_ADDRESS, MPU6886_TEMP_OUT_H, 2, buf);
+
+    *t = ((uint16_t)buf[0] << 8) | buf[1];
 }
 
 void MPU6886getGres()
 {
-   switch (Gyscale)
-   {
-  // Possible gyro scales (and their register bit settings) are:
-     case GFS_250DPS:
-           gRes = 250.0/32768.0;
-           break;
-     case GFS_500DPS:
-           gRes = 500.0/32768.0;
-           break;
-     case GFS_1000DPS:
-           gRes = 1000.0/32768.0;
-           break;
-     case GFS_2000DPS:
-           gRes = 2000.0/32768.0;
-           break;
-   }
+    switch (Gyscale)
+    {
+        // Possible gyro scales (and their register bit settings) are:
+    case GFS_250DPS:
+        gRes = 250.0 / 32768.0;
+        break;
+    case GFS_500DPS:
+        gRes = 500.0 / 32768.0;
+        break;
+    case GFS_1000DPS:
+        gRes = 1000.0 / 32768.0;
+        break;
+    case GFS_2000DPS:
+        gRes = 2000.0 / 32768.0;
+        break;
+    }
 }
-
 
 void MPU6886getAres()
 {
-   switch (Acscale)
-   {
-   // Possible accelerometer scales (and their register bit settings) are:
-   // 2 Gs (00), 4 Gs (01), 8 Gs (10), and 16 Gs  (11). 
-   // Here's a bit of an algorith to calculate DPS/(ADC tick) based on that 2-bit value:
+    switch (Acscale)
+    {
+        // Possible accelerometer scales (and their register bit settings) are:
+        // 2 Gs (00), 4 Gs (01), 8 Gs (10), and 16 Gs  (11).
+        // Here's a bit of an algorith to calculate DPS/(ADC tick) based on that 2-bit value:
     case AFS_2G:
-          aRes = 2.0/32768.0;
-          break;
+        aRes = 2.0 / 32768.0;
+        break;
     case AFS_4G:
-          aRes = 4.0/32768.0;
-          break;
+        aRes = 4.0 / 32768.0;
+        break;
     case AFS_8G:
-          aRes = 8.0/32768.0;
-          break;
+        aRes = 8.0 / 32768.0;
+        break;
     case AFS_16G:
-          aRes = 16.0/32768.0;
-          break;
-  }
+        aRes = 16.0 / 32768.0;
+        break;
+    }
 }
 
-void MPU6886setFIFOEnable( bool enableflag )
+void MPU6886setFIFOEnable(bool enableflag)
 {
-	uint8_t regdata = 0;
-	if( enableflag )
-	{
-		regdata = 0x0c;
-		MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_FIFO_ENABLE, 1, &regdata);
-		regdata = 0x40;
-		MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_USER_CTRL, 1, &regdata);
-		//MPU6886_FIFO_ENABLE = 0x0C
-		//MPU6886_USER_CTRL = 0x40
-	}
-	else
-	{
-		regdata = 0x00;
-		MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_FIFO_ENABLE, 1, &regdata);
-		regdata = 0x00;
-		MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_USER_CTRL, 1, &regdata);
-	}
+    uint8_t regdata = 0;
+    if (enableflag)
+    {
+        regdata = 0x0c;
+        MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_FIFO_ENABLE, 1, &regdata);
+        regdata = 0x40;
+        MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_USER_CTRL, 1, &regdata);
+        //MPU6886_FIFO_ENABLE = 0x0C
+        //MPU6886_USER_CTRL = 0x40
+    }
+    else
+    {
+        regdata = 0x00;
+        MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_FIFO_ENABLE, 1, &regdata);
+        regdata = 0x00;
+        MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_USER_CTRL, 1, &regdata);
+    }
 }
 
 uint8_t MPU6886ReadFIFO()
 {
-	 uint8_t ReData = 0;
-	 MPU6886I2C_Read_NBytes( MPU6886_ADDRESS , MPU6886_FIFO_R_W , 1 , &ReData );
-	 return ReData;    
+    uint8_t ReData = 0;
+    MPU6886I2C_Read_NBytes(MPU6886_ADDRESS, MPU6886_FIFO_R_W, 1, &ReData);
+    return ReData;
 }
 
-void MPU6886ReadFIFOBuff( uint8_t *DataBuff ,uint16_t Length )
+void MPU6886ReadFIFOBuff(uint8_t *DataBuff, uint16_t Length)
 {
-	 MPU6886I2C_Read_NBytes( MPU6886_ADDRESS , MPU6886_FIFO_R_W , Length , DataBuff );
+    MPU6886I2C_Read_NBytes(MPU6886_ADDRESS, MPU6886_FIFO_R_W, Length, DataBuff);
 }
 
- uint16_t MPU6886ReadFIFOCount()
- {
-	 uint8_t Buff[2];
-	 uint16_t ReData = 0;
-	 MPU6886I2C_Read_NBytes( MPU6886_ADDRESS , MPU6886_FIFO_CONUTH , 2 , Buff );
-	 ReData = Buff[0];
-	 ReData <<= 8;
-	 ReData |= Buff[1];
-	 return ReData;
- }
- 
+uint16_t MPU6886ReadFIFOCount()
+{
+    uint8_t Buff[2];
+    uint16_t ReData = 0;
+    MPU6886I2C_Read_NBytes(MPU6886_ADDRESS, MPU6886_FIFO_CONUTH, 2, Buff);
+    ReData = Buff[0];
+    ReData <<= 8;
+    ReData |= Buff[1];
+    return ReData;
+}
+
 void MPU6886SetGyroFsr(enum Gscale scale)
 {
     //return IIC_Write_Byte(MPU_GYRO_CFG_REG,scale<<3);//设置陀螺仪满量程范围
-    unsigned char regdata;	
-    regdata = (scale<<3);
+    unsigned char regdata;
+    regdata = (scale << 3);
     MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_GYRO_CONFIG, 1, &regdata);
-    vTaskDelay(10 /portTICK_PERIOD_MS);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
 
     Gyscale = scale;
     MPU6886getGres();
@@ -209,44 +208,43 @@ void MPU6886SetGyroFsr(enum Gscale scale)
 
 void MPU6886SetAccelFsr(enum Ascale scale)
 {
-    unsigned char regdata;	
-    regdata = (scale<<3);
+    unsigned char regdata;
+    regdata = (scale << 3);
     MPU6886I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_ACCEL_CONFIG, 1, &regdata);
-    vTaskDelay(10 /portTICK_PERIOD_MS);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
     Acscale = scale;
     MPU6886getAres();
 }
 
-void MPU6886getAccelData(float* ax, float* ay, float* az)
+void MPU6886getAccelData(float *ax, float *ay, float *az)
 {
-  int16_t accX = 0;
-  int16_t accY = 0;
-  int16_t accZ = 0;
-  MPU6886getAccelAdc(&accX,&accY,&accZ);
+    int16_t accX = 0;
+    int16_t accY = 0;
+    int16_t accZ = 0;
+    MPU6886getAccelAdc(&accX, &accY, &accZ);
 
-  *ax = (float)accX * aRes;
-  *ay = (float)accY * aRes;
-  *az = (float)accZ * aRes;
-
+    *ax = (float)accX * aRes;
+    *ay = (float)accY * aRes;
+    *az = (float)accZ * aRes;
 }
-      
-void MPU6886getGyroData(float* gx, float* gy, float* gz)
-{
-  int16_t gyroX = 0;
-  int16_t gyroY = 0;
-  int16_t gyroZ = 0;
-  MPU6886getGyroAdc(&gyroX,&gyroY,&gyroZ);
 
-  *gx = (float)gyroX * gRes;
-  *gy = (float)gyroY * gRes;
-  *gz = (float)gyroZ * gRes;
+void MPU6886getGyroData(float *gx, float *gy, float *gz)
+{
+    int16_t gyroX = 0;
+    int16_t gyroY = 0;
+    int16_t gyroZ = 0;
+    MPU6886getGyroAdc(&gyroX, &gyroY, &gyroZ);
+
+    *gx = (float)gyroX * gRes;
+    *gy = (float)gyroY * gRes;
+    *gz = (float)gyroZ * gRes;
 }
 
 void MPU6886getTempData(float *t)
 {
-  
-  int16_t temp = 0;
-  MPU6886getTempAdc(&temp);
-  
-  *t = (float)temp / 326.8 + 25.0;
+
+    int16_t temp = 0;
+    MPU6886getTempAdc(&temp);
+
+    *t = (float)temp / 326.8 + 25.0;
 }
--- main/main.c
+++ main/main.c
@@ -13,81 +13,166 @@
 #include "wire.h"
 #include "AXP192.h"
 
-static void stickc_test_task(void *arg)
+static uint8_t strbuff[50];
+static bool ButtonAFlag = false;
+
+static void LCDtest(void)
 {
-	char *strbuff = (char *)malloc(64);
-	uint16_t VbatData = 0;
-
-	float accX = 0;
-	float accY = 0;
-	float accZ = 0;
-
-	float gyroX = 0;
-	float gyroY = 0;
-	float gyroZ = 0;
-
-	float temp = 0;
-	while (1) {
-
-		MPU6886getGyroData(&gyroX,&gyroY,&gyroZ);
-		MPU6886getAccelData(&accX,&accY,&accZ);
-		MPU6886getTempData(&temp);
-
-		printf("%.2f   %.2f   %.2f      ", gyroX, gyroY,gyroZ);
-		printf("o/s\n");
-		printf("%.2f   %.2f   %.2f      ",accX * 1000,accY * 1000, accZ * 1000);
-		printf("mg\n");
-		printf("Temperature : %.2f C\n", temp);
-
-		TFT_print(" |", 5, 20);
-		TFT_print("Gyro", 30, 10);
-		TFT_print("| Accel", 80, 10);
-		TFT_print("X|", 5, 20);
-		TFT_print("Y|", 5, 30);
-		TFT_print("Z|", 5, 40);
-
-		sprintf(strbuff, "%.2f", gyroX);
-		TFT_print(strbuff, 30, 20);
-		sprintf(strbuff, "%.2f", gyroY);
-		TFT_print(strbuff, 30, 30);
-		sprintf(strbuff, "%.2f", gyroZ);
-		TFT_print(strbuff, 30, 40);
-
-		sprintf(strbuff, "| %.2f", accX * 1000);
-		TFT_print(strbuff, 80, 20);
-		sprintf(strbuff, "| %.2f", accY * 1000);
-		TFT_print(strbuff, 80, 30);
-		sprintf(strbuff, "| %.2f", accZ * 1000);
-		TFT_print(strbuff, 80, 40);
-
-		sprintf(strbuff, "Temperature:%.2fC\n", temp);
-		TFT_print(strbuff, 5, 50);
-
-		vTaskDelay(300 /portTICK_PERIOD_MS);
-
-	}
+    TFT_fillScreen(TFT_BLACK);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    TFT_fillScreen(TFT_RED);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    TFT_fillScreen(TFT_BLUE);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    TFT_fillScreen(TFT_GREEN);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    TFT_fillScreen(TFT_CYAN);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    TFT_fillScreen(TFT_YELLOW);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    TFT_fillScreen(TFT_WHITE);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    TFT_fillScreen(TFT_BLACK);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
 }
 
+static void AXP192Test(void)
+{
+    float Temp;
+    float BatVoltage;
+    float BatCurrent;
+    float VinVoltage;
+    float VinCurrent;
+    float VbusVoltage;
+
+    while( ButtonAFlag == false )
+    {
+        Temp = AXP192GetTempInAXP192( &wire0 );
+        sprintf( ( char* )strbuff, "AXP192-Temp:%.2fC", Temp);
+        TFT_print( ( char* )strbuff, 5, 10);
+        BatVoltage = AXP192GetBatVoltage( &wire0 );
+        sprintf( ( char* )strbuff, "BatVoltage:%.2fV", BatVoltage);
+        TFT_print( ( char* )strbuff, 5, 20);
+        BatCurrent = AXP192GetBatCurrent( &wire0 );
+        sprintf( ( char* )strbuff, "BatCurrent:%.2fmA", BatCurrent);
+        TFT_print(( char* )strbuff, 5, 30);
+        VinVoltage = AXP192GetVinVoltage( &wire0 );
+        sprintf( ( char* )strbuff, "VinVoltage:%.2fV", VinVoltage);
+        TFT_print(( char* )strbuff, 5, 40);
+        VinCurrent = AXP192GetVinCurrent( &wire0 );
+        sprintf( ( char* )strbuff, "VinCurrent:%.2fmA", VinCurrent);
+        TFT_print(( char* )strbuff, 5, 50);
+
+        vTaskDelay(200 / portTICK_PERIOD_MS);
+    }
+    ButtonAFlag  = false;
+    vTaskDelay(200 / portTICK_PERIOD_MS);
+    TFT_fillScreen(TFT_BLACK);
+    TFT_print(">>>M5 StickC<<<", CENTER, 0);
+}
+
+static void MPU6886Test(void)
+{
+    float accX = 0;
+    float accY = 0;
+    float accZ = 0;
+    float gyroX = 0;
+    float gyroY = 0;
+    float gyroZ = 0;
+    float temp = 0;
+
+    while ( ButtonAFlag == false )
+    {
+        MPU6886getGyroData(&gyroX, &gyroY, &gyroZ);
+        MPU6886getAccelData(&accX, &accY, &accZ);
+        MPU6886getTempData(&temp);
+
+        printf("%.2f   %.2f   %.2f      ", gyroX, gyroY, gyroZ);
+        printf("o/s\n");
+        printf("%.2f   %.2f   %.2f      ", accX * 1000, accY * 1000, accZ * 1000);
+        printf("mg\n");
+        printf("Temperature : %.2f C\n", temp);
+
+        TFT_print(" |", 5, 20);
+        TFT_print("Gyro", 30, 10);
+        TFT_print("| Accel", 80, 10);
+        TFT_print("X|", 5, 20);
+        TFT_print("Y|", 5, 30);
+        TFT_print("Z|", 5, 40);
+
+        sprintf( ( char* )strbuff, "%.2f", gyroX);
+        TFT_print(( char* )strbuff, 30, 20);
+        sprintf( ( char* )strbuff, "%.2f", gyroY);
+        TFT_print(( char* )strbuff, 30, 30);
+        sprintf( ( char* )strbuff, "%.2f", gyroZ);
+        TFT_print(( char* )strbuff, 30, 40);
+
+        sprintf( ( char* )strbuff, "| %.2f", accX * 1000);
+        TFT_print(( char* )strbuff, 80, 20);
+        sprintf( ( char* )strbuff, "| %.2f", accY * 1000);
+        TFT_print(( char* )strbuff, 80, 30);
+        sprintf( ( char* )strbuff, "| %.2f", accZ * 1000);
+        TFT_print(( char* )strbuff, 80, 40);
+
+        sprintf( ( char* )strbuff, "Temperature:%.2fC\n", temp);
+        TFT_print(( char* )strbuff, 5, 50);
+
+        vTaskDelay(200 / portTICK_PERIOD_MS);
+    }
+    vTaskDelay(200 / portTICK_PERIOD_MS);
+    ButtonAFlag  = false;
+    TFT_fillScreen(TFT_BLACK);
+    TFT_print(">>>M5 StickC<<<", CENTER, 0);
+}
+
+static void stickc_test_task(void *arg)
+{
+
+    while (1)
+    {
+        MPU6886Test();
+        AXP192Test();
+    }
+}
+
+void buttonEvent(void *handler_arg, esp_event_base_t base, int32_t id, void *event_data)
+{
+    if ((base == m5button_a.esp_event_base) && (id == M5BUTTON_BUTTON_CLICK_EVENT))
+    {
+        TFT_print("button A", CENTER, 60);
+        ButtonAFlag = true;
+    }
+
+    if ((base == m5button_b.esp_event_base) && (id == M5BUTTON_BUTTON_CLICK_EVENT))
+    {
+        TFT_print("button B", CENTER, 60);
+    }
+}
 
 void app_main(void)
 {
-	m5stickc_config_t m5config = M5STICKC_CONFIG_DEFAULT();
-	m5config.power.lcd_backlight_level = 3; // Set starting backlight level
+    m5stickc_config_t m5config = M5STICKC_CONFIG_DEFAULT();
+    m5config.power.lcd_backlight_level = 3; // Set starting backlight level
 
-	m5_init(&m5config);
+    m5_init(&m5config);
 
-	font_rotate = 0;
-	text_wrap = 0;
-	font_transparent = 0;
-	font_forceFixed = 0;
-	gray_scale = 0;
-	TFT_setGammaCurve(DEFAULT_GAMMA_CURVE);
-	TFT_setRotation(LANDSCAPE);
-	TFT_setFont(SMALL_FONT, NULL);
-	TFT_resetclipwin();
-	TFT_fillScreen(TFT_BLACK);
-	TFT_print(">>>M5 StickC<<<", CENTER, 0);
-	vTaskDelay(500 /portTICK_PERIOD_MS);
+    esp_event_handler_register_with(m5_event_loop, M5BUTTON_A_EVENT_BASE, M5BUTTON_BUTTON_CLICK_EVENT, buttonEvent, NULL);
+    esp_event_handler_register_with(m5_event_loop, M5BUTTON_B_EVENT_BASE, M5BUTTON_BUTTON_CLICK_EVENT, buttonEvent, NULL);
 
-	xTaskCreate(stickc_test_task, "stickc_test_task", 1024 * 2, (void *)0, 10, NULL);
+    font_rotate = 0;
+    text_wrap = 0;
+    font_transparent = 0;
+    font_forceFixed = 0;
+    gray_scale = 0;
+    TFT_setGammaCurve(DEFAULT_GAMMA_CURVE);
+    TFT_setRotation(LANDSCAPE);
+    TFT_setFont(SMALL_FONT, NULL);
+    TFT_resetclipwin();
+
+    LCDtest();
+
+    TFT_print(">>>M5 StickC<<<", CENTER, 0);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+
+    xTaskCreate(stickc_test_task, "stickc_test_task", 1024 * 2, (void *)0, 10, NULL);
 }
